fc707f41303db22a58ea611f31a344ce3a0ee0df
mpsanchezg <mapisanchezg@gmail.com>
2020-05-07 02:27:32 -0400

    First Project

---
+++ Model/EventModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarApp.Model
+{
+	class EventModel
+	{
+		private DateTime dateOfEvent;
+
+		public EventModel()
+		{
+
+		}
+
+		public DateTime DateOfEvent { get => dateOfEvent; set => dateOfEvent = value; }
+	}
+}
